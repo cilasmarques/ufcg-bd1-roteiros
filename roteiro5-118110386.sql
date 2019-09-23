edd92cc7ce03f88e03530935f0fab602bb9a5bd8
118110386 CILAS MEDEIROS DE FARIAS MARQUES <cilasmfm@lcc3-061.lcc.ufcg.edu.br>
2019-09-23 09:20:30 -0300

    .

--- roteiro5-118110386.sql
+++ roteiro5-118110386.sql
@@ -1,3 +1,5 @@
+--ssh -o ServerAliveInterval=30 cilas@150.165.15.11 -p 45600
+
 --Q1
 SELECT COUNT(e.ssn) FROM employee AS e WHERE e.sex = 'F';
 
@@ -14,10 +16,10 @@ SELECT s.fname AS nome_supervisor, COUNT(e.superssn) AS qtd_supervisionados FROM
 SELECT s.fname, COUNT(*) FROM (employee AS s RIGHT OUTER JOIN employee AS e ON (s.ssn = e.superssn)) GROUP BY (s.ssn) ORDER BY COUNT(*);
 
 --Q6
-SELECT MIN(qtd) FROM (SELECT count(*) AS qtd FROM works_on AS w GROUP BY w.pno) AS foo;
+SELECT MIN(qtd) FROM (SELECT COUNT(*) AS qtd FROM works_on AS w GROUP BY w.pno) AS foo;
 
 --Q7 +-
-SELECT MIN(num_projeto), MIN(qtd_func) FROM (SELECT p.pnumber AS num_projeto, count(*) AS qtd_func FROM works_on AS w JOIN project AS p ON (p.pnumber = w.pno)GROUP BY p.pnumber) AS foo;
+SELECT MIN(num_projeto), MIN(qtd_func) FROM (SELECT p.pnumber AS num_projeto, COUNT(*) AS qtd_func FROM works_on AS w JOIN project AS p ON (p.pnumber = w.pno)GROUP BY p.pnumber) AS foo;
 
 --Q8
 SELECT p.pnumber AS num_proj, AVG(e.salary) AS media_sal FROM project AS p, works_on AS w, employee AS e WHERE (p.pnumber = w.pno AND w.essn = e.ssn) GROUP BY p.pnumber;
@@ -25,3 +27,12 @@ SELECT p.pnumber AS num_proj, AVG(e.salary) AS media_sal FROM project AS p, work
 --Q9
 SELECT p.pnumber AS num_proj, p.pname AS proj_nome, AVG(e.salary) AS media_sal FROM project AS p, works_on AS w, employee AS e WHERE (p.pnumber = w.pno AND w.essn = e.ssn) GROUP BY p.pnumber;
 
+--Q10 deu preguiça de terminar
+--SELECT enotp92.fname, enotp92.salary FROM project AS p, works_on AS wnot92, works_on AS w92,employee AS enotp92, employee AS ep92 
+--WHERE (wnot92.pno != 92 AND wnot92.essn = enotp92.ssn AND w92.pno= 92 AND ep92.ssn = w92.essn );
+
+--Q11
+SELECT e.ssn, COUNT(w.essn) FROM employee AS e FULL OUTER JOIN works_on AS w ON (w.essn = e.ssn) GROUP BY(e.ssn) ORDER BY(COUNT(w.essn));
+
+--Q12 nao terminado
+SELECT p.pnumber, COUNT(e.ssn) FROM employee AS e ^Corks_on AS w ON (e.ssn = w.essn);
